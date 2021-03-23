1eebd394349e282703fdd69879665470de7b5569
Isabella <wileyisabella@gmail.com>
2021-03-23 17:40:12 -0400

    complete

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased(){
+  let newTutorials = tutorials.map(tutorial => {
+    let sentence = tutorial.split(" ")
+    let newSentence = sentence.map(word => {
+      return(word.charAt(0).toUpperCase() + word.slice(1))
+    })
+    return(newSentence.join(" "))
+  })
+  return newTutorials
 }
+
+
+// function titleCased(){
+//   let newTutorials = tutorials.map(tutorial => {
+//     let sentence = tutorial.split(" ");
+//     sentence.forEach(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     sentence.join(" ");
+//     return sentence;
+//   })
+//   return newTutorials;
+// }
+
+// const titleCased = tutorials.map(tutorial => {
+//   return(tutorial.charAt(0).toUpperCase() + tutorial.slice(1))
+// })
+
+// console.log(titleCased)
